b0ecb9321a22543d9c168cd3b314c3f103f7a879
XDDDDD <2513630371@qq.com>
2018-12-30 21:52:15 +0800

    Create 2.1.3.cpp

---
+++ 2.1.3 Literals/2.1.3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"Who goes with F\145rgus?\012";//prints Who goes with Fergus?
+										 //followed by a new line
+										 //\145 the 145 is octal digit
+	return 0;
+}
